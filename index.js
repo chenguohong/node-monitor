a4d6de2f4f5ea5b429c3fd0977b06a2b471df8c2
chenguohong <39668796+chenguohong@users.noreply.github.com>
2018-06-19 18:39:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,19 @@ nodeMonitor({
     dashboard: {
      port_http: 20000,
      port_tcp: 30000
-     }
+     },
+	 auth: {
+        need: true, // 需要鉴权
+        /**
+         @param {string} user 用户名
+         @param {string} pass 用户键入密码
+         @return {promise}
+         **/
+        authentication(user, pass) {
+            return new Promise(resolve => {
+                if ((user === 'admin' && pass === 'admin123')) resolve(true)
+                else resolve(false);
+            });
+        }
+    }
 })
